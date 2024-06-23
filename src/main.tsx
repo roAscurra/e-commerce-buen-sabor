2327afa56130bd41b25cea76277271af7eef0c27
roAscurra <rominaascurra4887@gmail.com>
2024-06-22 23:29:19 -0300

    Mod

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
-import {Auth0ProviderWithNavigate} from "./auth0/Auth0ProviderWithNavigate.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      {/* Envolvemos la aplicación en Provider y pasamos la tienda de Redux como prop */}
-      <Auth0ProviderWithNavigate>
           {/* Envolvemos la aplicación en Provider y pasamos la tienda de Redux como prop */}
           <Provider store={store}>
               <App /> {/* Renderizamos el componente principal de la aplicación */}
           </Provider>
-      </Auth0ProviderWithNavigate>
-
   </React.StrictMode>
 );
