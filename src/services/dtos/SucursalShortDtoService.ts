98683a1f41a2802e16b1447a21f5703b8347e2a6
roAscurra <rominaascurra4887@gmail.com>
2024-06-22 23:05:51 -0300

    mod

--- src/services/dtos/SucursalShortDtoService.ts
+++
@@ -1,5 +0,0 @@
-import SucursalShorDto from "../../types/dto/SucursalShortDto";
-import BackendClient from "../BackendClient";
-
-
-export default class SucursalShortDtoService extends BackendClient<SucursalShorDto> {}
