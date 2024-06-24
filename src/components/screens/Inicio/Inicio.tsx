4e8823d7d3a4de7ffac39b4246ffed82c5dbc759
roAscurra <rominaascurra4887@gmail.com>
2024-06-24 12:26:24 -0300

    Modificaciones

--- src/components/screens/Inicio/Inicio.tsx
+++ src/components/screens/Inicio/Inicio.tsx
@@ -3,13 +3,14 @@ import './Inicio.css'; // Archivo CSS para estilos personalizados
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faTags } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'; // Importa los componentes necesarios de React Bootstrap
+import { Container, Row, Col, Card, Button, Spinner } from 'react-bootstrap'; // Importa los componentes necesarios de React Bootstrap
 import Categoria from '../../../types/Categoria';
 import CategoriaService from '../../../services/CategoriaService';
 
 export const Inicio = () => {
   const url = import.meta.env.VITE_API_URL;
   const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [cargando, setCargando] = useState(true); // Estado para manejar la carga
   const categoriaService = new CategoriaService();
   localStorage.removeItem('categoriaSeleccionada');
 
@@ -21,16 +22,18 @@ export const Inicio = () => {
       } catch (error) {
         console.error('Error fetching categorias:', error);
       } finally {
-        console.log("")
+        setCargando(false); // Cambiar el estado de carga a false una vez que los datos se han obtenido
       }
     };
 
     fetchData();
   }, []);
+
   const handleClickCategoria = (categoriaId: number) => {
     localStorage.setItem('categoriaSeleccionada', categoriaId.toString());
-    window.location.href = '/carrito';
+    window.location.href = '/productos';
   };
+
   return (
     <Container fluid className="inicio-container">
       <Container className="contenido">
@@ -46,7 +49,7 @@ export const Inicio = () => {
                 </div>
                 <Card.Title>Productos</Card.Title>
                 <Card.Text>Descubre nuestra variedad de productos</Card.Text>
-                <Button className="Boton" href="/carrito">
+                <Button className="Boton" href="/productos">
                   Ver productos aquí
                 </Button>
               </Card.Body>
@@ -68,15 +71,26 @@ export const Inicio = () => {
           </Col>
         </Row>
         <Row className="categorias-container m-3">
-          {categorias.map((categoria) => (
-            <Col key={categoria.id} xs={6} md={4} lg={3} className="mb-4">
-              <Card className="tarjeta categoria-card h-100 text-center" onClick={() => handleClickCategoria(categoria.id)}>
-                <Card.Body className="d-flex flex-column justify-content-center">
-                  <Card.Title className="card-title h6">{categoria.denominacion}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          <div className="texto">
+            <h3 className='text-center'>Categorías</h3>
+          </div>
+          {cargando ? (
+            <div className="d-flex justify-content-center align-items-center w-100">
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Cargando...</span>
+              </Spinner>
+            </div>
+          ) : (
+            categorias.map((categoria) => (
+              <Col key={categoria.id} xs={6} md={4} lg={3} className="mb-4">
+                <Card className="tarjeta categoria-card h-100 text-center" onClick={() => handleClickCategoria(categoria.id)}>
+                  <Card.Body className="d-flex flex-column justify-content-center">
+                    <Card.Title className="card-title h6">{categoria.denominacion}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
         </Row>
       </Container>
       <hr />
