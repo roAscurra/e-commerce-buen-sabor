40ac0550c7c1a01fc7947056c876c999eef6da03
roAscurra <rominaascurra4887@gmail.com>
2024-06-24 00:24:38 -0300

    coreccion deploy

--- src/components/screens/Inicio/Inicio.tsx
+++ src/components/screens/Inicio/Inicio.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Inicio.css'; // Archivo CSS para estilos personalizados
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faTags } from '@fortawesome/free-solid-svg-icons';
